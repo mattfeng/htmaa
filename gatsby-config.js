f17c39c92ee5726f16a50900369d0176d2bab176
Matthew Feng <mattfeng@mit.edu>
2021-09-16 17:52:52 -0400

    starting fresh

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,10 @@
 module.exports = {
+  pathPrefix: `/classes/MAS.863/EECS/people/mattfeng`,
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `/mattfeng/how to make almost anything`,
+    description: `how to make almost anything fall 2021`,
+    author: `@mattfeng`,
+    siteUrl: `http://fab.cba.mit.edu/classes/MAS.863/EECS/people/mattfeng/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -17,20 +18,5 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
---
+++ src/pages/test.js
@@ -0,0 +1,9 @@
+import * as React from "react"
+
+const TestPage = () => {
+    return <>
+        <h2>this is a test, very cool</h2>
+    </>
+}
+
+export default TestPage
